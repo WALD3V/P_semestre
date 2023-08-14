fe317a4b65c6249b851e73ff59df8c8f49f29b5c
walterd <walter.villamarr@ug.edu.ec>
2023-08-14 12:50:49 -0500

    subida e portada

--- P_semestre/Form1.Designer.cs
+++ P_semestre/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            monthCalendar1 = new MonthCalendar();
+            textBox1 = new TextBox();
+            splitContainer1 = new SplitContainer();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            monthCalendar1.Location = new Point(141, 136);
+            monthCalendar1.Name = "monthCalendar1";
+            monthCalendar1.TabIndex = 0;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(141, 101);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(248, 23);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "Search";
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(12, 12);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(button3);
+            splitContainer1.Panel1.Controls.Add(button2);
+            splitContainer1.Panel1.Controls.Add(button1);
+            splitContainer1.Panel1.Paint += splitContainer1_Panel1_Paint;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(label2);
+            splitContainer1.Panel2.Controls.Add(label1);
+            splitContainer1.Panel2.Controls.Add(monthCalendar1);
+            splitContainer1.Panel2.Controls.Add(textBox1);
+            splitContainer1.Panel2.Paint += splitContainer1_Panel2_Paint;
+            splitContainer1.Size = new Size(776, 435);
+            splitContainer1.SplitterDistance = 258;
+            splitContainer1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 101);
+            button1.Name = "button1";
+            button1.Size = new Size(176, 23);
+            button1.TabIndex = 0;
+            button1.Text = "EVENTOS ";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(39, 151);
+            button2.Name = "button2";
+            button2.Size = new Size(176, 23);
+            button2.TabIndex = 0;
+            button2.Text = "button1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(39, 196);
+            button3.Name = "button3";
+            button3.Size = new Size(176, 23);
+            button3.TabIndex = 0;
+            button3.Text = "button1";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(142, 317);
+            label1.Name = "label1";
+            label1.Size = new Size(104, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Detalle del evento ";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(141, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Buscar Evento :";
+            label2.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(splitContainer1);
+            Name = "Form1";
+            Text = "HOME";
+            Load += Form1_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+        private MonthCalendar monthCalendar1;
+        private TextBox textBox1;
+        private SplitContainer splitContainer1;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label label1;
+        private Label label2;
     }
 }
--- P_semestre/Form1.cs
+++ P_semestre/Form1.cs
@@ -6,5 +6,45 @@ namespace P_semestre
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
